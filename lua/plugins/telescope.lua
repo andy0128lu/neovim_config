112f87587bb99a73eac906adc630f2d831945e85
andy0128lu <andy0128.lu@gmail.com>
2024-02-05 16:56:05 +1100

    Updated plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,25 @@
-return  {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.5', 
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function() 
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- Set shortcut "space + ff" to find files based on keyword
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- Set shortcut "space + fg" to grap files through projects
-  end
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {}) -- Set shortcut "space + ff" to find files based on keyword
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) -- Set shortcut "space + fg" to grap files through projects
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,19 +4,7 @@ return {
   config = function()
     local treesitterConfig = require("nvim-treesitter.configs")
     treesitterConfig.setup({
-      ensure_installed = { 
-        "lua", 
-        "javascript", 
-        "typescript", 
-        "ruby", 
-        "bash", 
-        "markdown", 
-        "html", 
-        "css", 
-        "json", 
-        "c_sharp", 
-        "sql" 
-      },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true }
     })
