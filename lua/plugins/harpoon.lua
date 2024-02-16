c1a2bc12aaa6ec55f7b86075e5d5d60e6913cb77
andy0128lu <andy0128.lu@gmail.com>
2024-02-16 15:58:14 +1100

    Added harpoon plugin for quickly switching between buffers

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,81 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+
+		-- REQUIRED
+		harpoon:setup({
+			settings = {
+				save_on_toggle = true,
+			},
+		})
+
+		harpoon:extend({
+			UI_CREATE = function(cx)
+				vim.keymap.set("n", "<leader>hv", function()
+					harpoon.ui:select_menu_item({ vsplit = true })
+				end, { buffer = cx.bufnr })
+			end,
+		})
+
+		-- basic telescope configuration
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
+		end
+
+		vim.keymap.set("n", "<leader>hf", function()
+			toggle_telescope(harpoon:list())
+		end, { desc = "Search harpoon file in telescope window" })
+
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():append()
+		end, {})
+		vim.keymap.set("n", "<leader>hl", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, {})
+
+		vim.keymap.set("n", "<leader>h1", function()
+			harpoon:list():select(1)
+		end, {})
+		vim.keymap.set("n", "<leader>h2", function()
+			harpoon:list():select(2)
+		end, {})
+		vim.keymap.set("n", "<leader>h3", function()
+			harpoon:list():select(3)
+		end, {})
+		vim.keymap.set("n", "<leader>h4", function()
+			harpoon:list():select(4)
+		end, {})
+		vim.keymap.set("n", "<leader>h5", function()
+			harpoon:list():select(5)
+		end, {})
+		vim.keymap.set("n", "<leader>h6", function()
+			harpoon:list():select(6)
+		end, {})
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<leader>h[", function()
+			harpoon:list():prev()
+		end, {})
+		vim.keymap.set("n", "<leader>h]", function()
+			harpoon:list():next()
+		end, {})
+	end,
+}
