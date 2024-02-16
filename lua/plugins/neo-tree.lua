6328dec006aaad6ce82e2009dadd5f0b55eab974
andy0128lu <andy0128.lu@gmail.com>
2024-02-16 16:00:19 +1100

    Updated lazy-lock.json

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,22 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function(Plugin, opts)
+		--require("neo-tree").setup({
+		-- Tried to enable opeing neotree window by default but it also changed other default behaviour
+		-- filesystem = {
+		--    filtered_items = {
+		--      visible = true, -- when true, they will just be displayed differently than normal items
+		--      hide_dotfiles = false,
+		--      hide_gitignored = false,
+		--    }
+		--}
+		--})
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
+	end,
 }
