b3d0948d3fd9c807d2796248755b0515f828560b
andy0128lu <andy0128.lu@gmail.com>
2024-03-05 21:42:04 +1100

    Added new keymap

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,6 +12,7 @@ return {
     "tpope/vim-fugitive",
     config = function()
       vim.keymap.set("n", "<leader>gs", ":Git<CR>", {})
+      vim.keymap.set("n", "<leader>gh", ":Git blame<CR>", {})
     end,
   },
 }
