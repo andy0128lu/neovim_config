6fb6fea4b03c0a8cba874ef50824f4817e22d582
andy0128lu <andy0128.lu@gmail.com>
2024-03-18 17:12:58 +1100

    Removed unused keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.g.mapleader = " "
+--vim.opt.foldmethod = "indent"
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -12,8 +13,5 @@ vim.opt.conceallevel = 2 -- for UI features of obsidian.nvim
 vim.keymap.set("n", ";", ":", { desc = "Swap ; and :" })
 vim.keymap.set("n", ":", ";", { desc = "Swap ; and :" })
 vim.keymap.set("n", "<leader>hn", ":nohlsearch<CR>", { desc = "Hide highlight in search result" })
-
-vim.keymap.set("n", "<leader>nt","gt", { desc = "Next tab" })
-vim.keymap.set("n", "<leader>pt","gT", { desc = "Previous tab" })
 
 vim.keymap.set("n", "<C-t>", ":terminal<CR>", { desc = "Open terminal mode" })
