a6df800999b90997c4be5c97430d9450389b7497
andy0128lu <andy0128.lu@gmail.com>
2024-03-22 22:17:48 +1100

    Added searching symbols in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,37 +1,43 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {}) -- Set shortcut "space + ff" to find files based on keyword
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) -- Set shortcut "space + fg" to grap files through projects
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {}) -- Set shortcut "space + fb" to get the buffers list
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				pickers = {
-					find_files = {
-						find_command = { "rg", "--files", "--glob", "!**/.git/*", "--hidden" },
-					},
-					grep_string = {
-						additional_args = { "--hidden" },
-					},
-					live_grep = {
-						additional_args = { "--hidden" },
-					},
-					extensions = {
-						["ui-select"] = {
-							require("telescope.themes").get_dropdown({}),
-						},
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find files based on keyword" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Search in the current entire directory" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Search in the current buffer" })
+      vim.keymap.set(
+        "n",
+        "<leader>fs",
+        builtin.treesitter,
+        { desc = "Lists Function names, variables from Treesitter" }
+      )
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            find_command = { "rg", "--files", "--glob", "!**/.git/*", "--hidden" },
+          },
+          grep_string = {
+            additional_args = { "--hidden" },
+          },
+          live_grep = {
+            additional_args = { "--hidden" },
+          },
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_dropdown({}),
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
