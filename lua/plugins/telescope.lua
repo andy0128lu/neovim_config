a8694417a818d41b3f81f02ebae1d58f59caa181
andy0128lu <andy0128.lu@gmail.com>
2024-04-14 18:42:25 +1000

    Added telescope with args and mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,15 +2,80 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    },
+    opts = function(_, opts)
+      local lga_actions = require("telescope-live-grep-args.actions")
+      opts.extensions = {
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+            n = {
+              -- TODO: live_grep on the first live_grep result but doesn't seem working?
+              ["<C-r>"] = {
+                function(p_bufnr)
+                  -- send results to quick fix list
+                  require("telescope.actions").send_to_qflist(p_bufnr)
+                  local qflist = vim.fn.getqflist()
+                  local paths = {}
+                  local hash = {}
+                  for k in pairs(qflist) do
+                    local path = vim.fn.bufname(qflist[k]["bufnr"]) -- extract path from quick fix list
+                    if not hash[path] then                -- add to paths table, if not already appeared
+                      paths[#paths + 1] = path
+                      hash[path] = true                   -- remember existing paths
+                    end
+                  end
+                  -- show search scope with message
+                  vim.notify("find in ...\n  " .. table.concat(paths, "\n  "))
+                  -- execute live_grep_args with search scope
+                  require("telescope").extensions.live_grep_args.live_grep_args({
+                    search_dirs = paths,
+                  })
+                end,
+                type = "action",
+                opts = {
+                  nowait = true,
+                  silent = true,
+                  desc = "Live grep on results",
+                },
+              },
+            },
+          },
+        },
+      }
+      return opts
+    end,
+    config = function(_, opts)
+      local telescope = require("telescope")
       local builtin = require("telescope.builtin")
+      telescope.setup(opts)
+      telescope.load_extension("live_grep_args")
+
       local function grep_cword()
-        return builtin.grep_string({ search = vim.fn.expand("<cword>") })
+        local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+        return live_grep_args_shortcuts.grep_word_under_cursor()
       end
+
+      local function live_grep_args()
+        local lga = telescope.extensions.live_grep_args
+        return lga.live_grep_args()
+      end
+
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find files based on keyword" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Search in the current entire directory" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Search in the current buffer" })
+      vim.keymap.set("n", "<leader>fg", live_grep_args, { desc = "Search in the current entire directory" })
       vim.keymap.set("n", "<leader>fc", grep_cword, { desc = "Search in the current buffer" })
       vim.keymap.set(
         "n",
@@ -23,7 +88,8 @@ return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require("telescope").setup({
+      local telescope = require("telescope")
+      telescope.setup({
         pickers = {
           find_files = {
             find_command = { "rg", "--files", "--glob", "!**/.git/*", "--hidden" },
@@ -41,7 +107,7 @@ return {
           },
         },
       })
-      require("telescope").load_extension("ui-select")
+      telescope.load_extension("ui-select")
     end,
   },
 }
