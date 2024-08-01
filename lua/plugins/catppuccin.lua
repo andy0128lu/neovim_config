d266b94d24f8ae319de3c78489d402477db1142d
andy0128lu <andy0128.lu@gmail.com>
2024-08-01 17:12:25 +1000

    Fixed opts not taken in catpuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,7 +4,8 @@ return {
   name = "catppuccin",
   priority = 1000,
   opts = { flavour = "mocha" }, -- latte, frappe, macchiato, mocha
-  config = function()
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
   end,
 }
