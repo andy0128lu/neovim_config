dea576592a57fcdc558b935227b0b6bea6c21f3f
andy0128lu <andy0128.lu@gmail.com>
2024-08-28 13:20:16 +1000

    Added keymap for viewing a PR

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,6 +2,7 @@ return {
   'sindrets/diffview.nvim',
   keys = {
     { mode = { "n", "i" }, "<leader>vo", "<cmd>DiffviewOpen<cr>", desc = "Open diff view" },
+    { mode = { "n", "i" }, "<leader>vp", "<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<cr>", desc = "Compare the header of current branch with the remote base branch (PR view)" },
     { mode = { "n", "i" }, "<leader>vb", "<cmd>DiffviewFileHistory<cr>", desc = "[V]iew file history for the current [b]ranch" },
     { mode = { "n", "i" }, "<leader>vf", "<cmd>DiffviewFileHistory %<cr>", desc = "[V]iew file history for the current [f]ile" },
   }
