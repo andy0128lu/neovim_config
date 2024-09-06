6397f0d735b137548ae60c43fc22b8a567acdd03
andy0128lu <andy0128.lu@gmail.com>
2024-09-06 15:26:38 +1000

    feat: config treeview and removed pinned version

--- lua/plugins/bookmarks.lua
+++ lua/plugins/bookmarks.lua
@@ -1,14 +1,41 @@
 return {
   "LintaoAmons/bookmarks.nvim",
-  tag = "v0.5.3", -- optional, pin the plugin at specific version for stability
   dependencies = {
     { "nvim-telescope/telescope.nvim" },
     { "stevearc/dressing.nvim" } -- optional: to have the same UI shown in the GIF
   },
+  opts = {
+    picker = {
+      -- choose built-in sort logic by name: string, find all the sort logics in `bookmarks.adapter.sort-logic`
+      -- or custom sort logic: function(bookmarks: Bookmarks.Bookmark[]): nil
+      sort_by = "last_visited",
+    },
+    treeview = {
+      bookmark_format = function(bookmark)
+        return bookmark.name ..
+        " [" .. bookmark.location.project_name .. "] " .. bookmark.location.relative_path .. " : " .. bookmark.content
+      end,
+      keymap = {
+        quit = { "q", "<ESC>" },
+        refresh = "R",
+        create_folder = "a",
+        tree_cut = "x",
+        tree_paste = "p",
+        collapse = "o",
+        delete = "d",
+        active = "s",
+        copy = "c",
+      },
+    },
+  },
   keys = {
-    { mode = { "n", "v" }, "<leader>mm", "<cmd>BookmarksMark<cr>",       desc = "Mark current line into active BookmarkList." },
-    { mode = { "n", "v" }, "<leader>ml", "<cmd>BookmarksGoto<cr>",       desc = "Go to bookmark at current active BookmarkList" },
-    { mode = { "n", "v" }, "<leader>ma", "<cmd>BookmarksCommands<cr>",   desc = "Find and trigger a bookmark command.([m]ark [a]ll)" },
-    { mode = { "n", "v" }, "<leader>mp", "<cmd>BookmarksGotoRecent<cr>", desc = "Go to latest visited/created Bookmark ([m]ark [p]revious" }
-  }
+    { mode = { "n", "v" }, "<leader>mm", "<cmd>BookmarksMark<cr>",        desc = "Mark current line into active BookmarkList." },
+    { mode = { "n", "v" }, "<leader>ml", "<cmd>BookmarksGoto<cr>",        desc = "Go to bookmark at current active BookmarkList" },
+    { mode = { "n", "v" }, "<leader>ma", "<cmd>BookmarksCommands<cr>",    desc = "Find and trigger a bookmark command.([m]ark [a]ll)" },
+    { mode = { "n", "v" }, "<leader>mp", "<cmd>BookmarksGotoRecent<cr>",  desc = "Go to latest visited/created Bookmark ([m]ark [p]revious" },
+    { mode = { "n", "v" }, "<leader>mt", "<cmd>BookmarksTree<cr>",        desc = "Open tree view" }
+  },
+  config = function(_, opts)
+    require("bookmarks").setup(opts);
+  end
 }
