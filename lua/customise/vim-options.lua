64c7a1ad1daa7956f046a35e7445215e57480a06
andy0128lu <andy0128.lu@gmail.com>
2024-10-17 16:29:12 +1100

    feat: add keymaps for moving lines

--- lua/customise/vim-options.lua
+++ lua/customise/vim-options.lua
@@ -21,6 +21,9 @@ vim.keymap.set("n", ";", ":", { desc = "Swap ; and :" })
 vim.keymap.set("n", ":", ";", { desc = "Swap ; and :" })
 vim.keymap.set("n", "<leader>hn", ":nohlsearch<CR>", { desc = "Hide highlight in search result" })
 vim.keymap.set("n", "<leader>ct", ":tabclose<CR>", { desc = "Close all windows in the current tab" })
+--- Move selected lines up and down (Aligning with the indent for the given block, such as within brackets)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-t>", ":terminal<CR>", { desc = "Open terminal mode" })
 
