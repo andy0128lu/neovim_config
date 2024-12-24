111e46bab03c39a99364832092885548739c8387
andy0128lu <andy0128.lu@gmail.com>
2024-12-24 13:28:40 +1100

    feat: formating on save

--- lua/customise/vim-options.lua
+++ lua/customise/vim-options.lua
@@ -16,5 +16,20 @@ vim.opt.conceallevel = 2 -- for UI features of obsidian.nvim
 -- deriving colours from the user's theme, you can change highlight groups by
 vim.opt.termguicolors = true
 
+-- Handlers
+--- BufWritePre
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        -- Formatting on Save
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
+
 
 
