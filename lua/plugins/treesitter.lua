436e1cc679f4812db21a70c96d8e0a5386dd3c3d
andy0128lu <andy0128.lu@gmail.com>
2025-02-13 16:14:59 +1100

    feat: plugins updated

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,16 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local treesitterConfig = require("nvim-treesitter.configs")
-		treesitterConfig.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  commit = "49f1b9a",
+  config = function()
+    local treesitterConfig = require("nvim-treesitter.configs")
+    treesitterConfig.setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      --update_focused_file = {
+      --  enable = true
+      --}
+    })
+  end,
 }
