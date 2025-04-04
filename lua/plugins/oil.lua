66a387298ffcca64dae0e92cf2b3cc6b84ba0349
Andy Lu <andy0128.lu@gmail.com>
2025-04-04 21:41:52 +1100

    feat: updated config for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,35 @@ return {
   opts = {
     -- Set to true to watch the filesystem for changes and reload oil
     watch_for_changes = false,
+    -- Id is automatically added at the beginning, and name at the end
+    -- See :help oil-columns
+    columns = {
+      "icon",
+      "permissions",
+      --"size",
+      --"mtime",
+    },
+    -- Configuration for the floating window in oil.open_float
+    float = {
+      -- Padding around the floating window
+      padding = 8,
+      -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      max_width = 0,
+      max_height = 0,
+      border = "rounded",
+      win_options = {
+        winblend = 0,
+      },
+      -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+      get_win_title = nil,
+      -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+      preview_split = "auto",
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      override = function(conf)
+        return conf
+      end,
+    },
   },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
@@ -12,7 +41,10 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   config = function(_, opts)
-    require("oil").setup(opts)
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    local oil = require("oil")
+    --vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "<leader>-", oil.toggle_float, { desc = "Toggle Oil in floating window" })
+
+    oil.setup(opts)
   end
 }
