0e69ff3ba69cb9921d94e245839811828918b47d
andy0128lu <andy0128.lu@gmail.com>
2026-05-27 16:12:46 +1000

    `feat: add keybinding to close diffview`

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,6 +2,7 @@ return {
   'sindrets/diffview.nvim',
   keys = {
     { mode = { "n" }, "<leader>vo", "<cmd>DiffviewOpen<cr>", desc = "Open diff view" },
+    { mode = { "n" }, "<leader>vc", "<cmd>DiffviewClose<cr>", desc = "Close diff view" },
     { mode = { "n" }, "<leader>vp", "<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<cr>", desc = "Compare the header of current branch with the remote base branch (PR view)" },
     { mode = { "n" }, "<leader>vb", "<cmd>DiffviewFileHistory<cr>", desc = "[V]iew file history for the current [b]ranch" },
     { mode = { "n" }, "<leader>vf", "<cmd>DiffviewFileHistory %<cr>", desc = "[V]iew file history for the current [f]ile" },
