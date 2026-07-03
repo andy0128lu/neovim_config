4f4a7417f73cad8c207d03c17312aa0afdb9070b
andy0128lu <andy0128.lu@gmail.com>
2026-07-03 15:55:45 +1000

    feat: supports switching master or main

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,7 +14,12 @@ return {
       vim.keymap.set("n", "<leader>gs", ":tab Git<CR>", {})
       vim.keymap.set("n", "<leader>gh", ":0Gclog<CR>", {})
       vim.keymap.set("n", "<leader>gv", ":Gvdiffsplit<CR>", {})
-      vim.keymap.set("n", "<leader><leader>gm", ":Git switch master<CR>", {})
+      vim.keymap.set("n", "<leader><leader>gm", function()
+        -- Try to get the SHA of main. It returns 0 and prints SHA, or returns 128 if doesn't exist
+        vim.fn.system("git rev-parse --verify main 2>/dev/null")
+        local branch = vim.v.shell_error == 0 and "main" or "master"
+        vim.cmd("Git switch " .. branch)
+      end, {})
       vim.keymap.set("n", "<leader><leader>g-", ":Git switch -<CR>", {})
       vim.keymap.set("n", "<leader><leader>gr", ":Git reset --soft HEAD~1<CR>", {})
       vim.keymap.set("n", "<leader><leader>gpl", ":Git pull<CR>", {})
