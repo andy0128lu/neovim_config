1d77c8627d4fd138958c01897d8dc8a9c9897611
andy0128lu <andy0128.lu@gmail.com>
2026-08-14 09:33:35 +1000

    chore: disabled treesitter for csv

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,10 @@ return {
     local treesitterConfig = require("nvim-treesitter.configs")
     treesitterConfig.setup({
       auto_install = true,
-      highlight = { enable = true },
+      highlight = { 
+        enable = true,
+        disable = { "csv" }
+      },
       indent = { enable = true },
       --update_focused_file = {
       --  enable = true
